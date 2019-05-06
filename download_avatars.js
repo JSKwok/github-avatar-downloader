77bc3634f35cdbd7c2c3c0ed5b698f5da7cce826
Justin <justin.s.kwok@gmail.com>
2019-05-06 21:24:50 +0000

    Created main project file, required necessary modules, created function with hard-coded values

---
+++ download_avatars.js
@@ -0,0 +1,14 @@
+var request = require('request');
+var fs = require('fs');
+
+console.log('Welcome to the GitHub Avatar Downloader!');
+
+
+function getRepoContributors (repoOwner, repoName, cb) {
+
+}
+
+getRepoContributors("jquery", "jquery", function (err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
