e8c7733eed6043088bffd57f45896483ac9af3ff
Justin <justin.s.kwok@gmail.com>
2019-05-06 23:18:21 +0000

    Added token to gitignore, program now lists the avatars of repo contributors in the target

--- download_avatars.js
+++ download_avatars.js
@@ -1,14 +1,27 @@
 var request = require('request');
 var fs = require('fs');
+var secrets = require('./secrets.js');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-
-function getRepoContributors (repoOwner, repoName, cb) {
-
+// Declare the function.
+function getRepoContributors (repoOwner, repoName, callback) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      Authorization : 'token ' + secrets.GITHUB_TOKEN
+    }
+  }
+  request(options, function (err, res, body) {
+    callback(err, JSON.parse(body));
+  });
 }
-
+// Call the function, using a callback as an argument.
+// The callback tells us what the program will do with the body
 getRepoContributors("jquery", "jquery", function (err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  for (let i = 0; i < result.length; i++) {
+    console.log("Results:", result[i].avatar_url);
+  }
 });
