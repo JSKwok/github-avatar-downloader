b1fb24c236ae459ae98a08bba4fbbccfefbccf72
Justin <justin.s.kwok@gmail.com>
2019-05-07 00:23:17 +0000

    Combined functions, loops through URLs to download images in jpeg format to specified folder

--- download_avatars.js
+++ download_avatars.js
@@ -5,31 +5,34 @@ var secrets = require('./secrets.js');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 // Declare the function.
-// function getRepoContributors (repoOwner, repoName, callback) {
-//   var options = {
-//     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-//     headers: {
-//       'User-Agent': 'request',
-//       Authorization : 'token ' + secrets.GITHUB_TOKEN
-//     }
-//   }
-//   request(options, function (err, res, body) {
-//     callback(err, JSON.parse(body));
-//   });
-// }
-// // Call the function, using a callback as an argument.
-// // The callback tells us what the program will do with the body
-// getRepoContributors("jquery", "jquery", function (err, result) {
-//   console.log("Errors:", err);
-//   for (let i = 0; i < result.length; i++) {
-//     console.log("Results:", result[i].avatar_url);
-//   }
-// });
+function getRepoContributors (repoOwner, repoName, callback) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      Authorization : 'token ' + secrets.GITHUB_TOKEN
+    }
+  }
+  request(options, function (err, res, body) {
+    callback(err, JSON.parse(body));
+  });
+}
+// Call the function, using a callback as an argument.
+// The callback tells us what the program will do with the body
+getRepoContributors("jquery", "jquery", function (err, result) {
+  console.log("Errors:", err);
+  for (let i = 0; i < result.length; i++) {
+    console.log(result[i].avatar_url)
+    if (!fs.existsSync('./avatars/')) {
+      fs.mkdirSync('./avatars/');
+    }
+    downloadImageByURL(result[i].avatar_url, './avatars/' + `${result[i].login}` + '.jpg');
+  }
+});
 
 function downloadImageByURL(url, filePath) {
   request.get(url)
     .pipe(fs.createWriteStream(filePath));
 }
 
-downloadImageByURL('https://avatars1.githubusercontent.com/u/43004?v=4', './image.jpg' )
 
