fda1359e1ebc7a8242db519146176a8fa330caae
Justin <justin.s.kwok@gmail.com>
2019-05-07 04:47:54 +0000

    Added comments

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,20 @@
 var request = require('request');
 var fs = require('fs');
 var secrets = require('./secrets.js');
+var repoOwner = process.argv[2];
+var repoName = process.argv[3];
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-// Declare the function.
 function getRepoContributors (repoOwner, repoName, callback) {
+
+  // Return an error message if either argument is empty.
+  if (repoOwner == undefined || repoName == undefined) {
+    console.log("Error: Please input both an owner and a name to download contributors for this repo.")
+    return;
+  };
+
+  // Identify the endpoint where the data is located and authorize access
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
@@ -13,26 +22,39 @@ function getRepoContributors (repoOwner, repoName, callback) {
       Authorization : 'token ' + secrets.GITHUB_TOKEN
     }
   }
+
+  // Attempt to GET data. Throws an error if the path is invalid.
   request(options, function (err, res, body) {
+    console.log('Status: ', res.statusMessage);
+    res.on('error', function (err) {
+      throw err;
+    });
+  // Calls the callback function if no error has occurred.
     callback(err, JSON.parse(body));
   });
 }
-// Call the function, using a callback as an argument.
-// The callback tells us what the program will do with the body
-getRepoContributors("jquery", "jquery", function (err, result) {
-  console.log("Errors:", err);
+
+// Calls the main function using arguments fropm command line.
+getRepoContributors(repoOwner, repoName, function (err, result) {
+  // Create target directory if it does not exist.
+  if (!fs.existsSync('./avatars/')) {
+    fs.mkdirSync('./avatars/');
+  };
+  // Loop through the resulting array.
   for (let i = 0; i < result.length; i++) {
-    console.log(result[i].avatar_url)
-    if (!fs.existsSync('./avatars/')) {
-      fs.mkdirSync('./avatars/');
-    }
+    // Download specified image in loop to target directory.
     downloadImageByURL(result[i].avatar_url, './avatars/' + `${result[i].login}` + '.jpg');
-  }
+  };
 });
 
+// Takes the URL from the scrape and writes to the specified file path.
 function downloadImageByURL(url, filePath) {
   request.get(url)
-    .pipe(fs.createWriteStream(filePath));
+    .pipe(fs.createWriteStream(filePath))
+    .on('finish', function () {
+      console.log("Image downloaded.")
+    });
 }
+
 
 
